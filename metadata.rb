cc25224465c67af95df963ae1dada52524e0094a
paulczar <paul@paulcz.net>
2013-11-24 09:33:51 -0600

    metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,16 @@ name             'YOUR_COOKBOOK'
 maintainer       'YOUR_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
-description      'Installs/Configures ircaas'
+description      'Installs/Configures APPLICATION'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+recipe "YOUR_COOKBOOK::default", "default cookbook"
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
+
+%w{ }.each do |dep|
+  depends dep
+end
